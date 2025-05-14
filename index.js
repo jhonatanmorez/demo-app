09e527257f03f811910e304a3bfedd156e38b5b4
jhonatanmorez <jhonatan.more5@hotmail.com>
2025-05-14 13:38:39 -0500

    Update

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // index.js
-console.log("Helo from Dockerized App!");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Dockerized App!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
